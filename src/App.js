f5ae7dc300d6824015eb9c456ffeb8c2ea6f37f5
Naima Ghulam Muhammad <naimaghm92@gmail.com>
2021-04-26 21:24:04 +0500

    complete project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from "react";
+import Dashboard from "./views/Dashboard";
+import 'antd/dist/antd.css'; 
+import {
+  BrowserRouter as Router
+} from "react-router-dom"
 
-function App() {
+const App = () => {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Dashboard/>
+    </Router>
+ 
+  )
 }
 
 export default App;
